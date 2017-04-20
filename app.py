ac0f81e05ccaef7fa59a05e17b978dae4f4f9c9a
woannaju <jwu@utexas.edu>
2017-04-20 01:06:39 -0500

    implementing flask for individual pages

--- app.py
+++ app.py
@@ -34,5 +34,78 @@ def parks():
 def parktypes():
 	return render_template('park_types.html')
 
+@app.route('/anzaborrego')
+def anzaborrego():
+	return render_template('Anza-Borrego_park.html')
+
+@app.route('/bigbend')
+def bigbend():
+	return render_template('bigbend_park.html')
+
+@app.route('/california')
+def california():
+	return render_template('California.html')
+
+@app.route('/centralpark')
+def centralpark():
+	return render_template('Central_Park.html')
+
+@app.route('/florida')
+def florida():
+	return render_template('Florida.html')
+
+@app.route('/historictrail')
+def historictrail():
+	return render_template('Historic_Trail.html')
+
+@app.route('/monument')
+def monument():
+	return render_template('Monument.html')
+
+@app.route('/nationalpark')
+def nationalpark():
+	return render_template('National_Park.html')
+
+@app.route('/newyork')
+def newyork():
+	return render_template('New York.html')
+
+@app.route('/preserve')
+def preserve():
+	return render_template('Preserve.html')
+
+@app.route('/statepark')
+def statepark():
+	return render_template('State_Park.html')
+
+@app.route('/texas')
+def texas():
+	return render_template('Texas.html')
+
+@app.route('/timucuanpark')
+def timucuanpark():
+	return render_template('Timuncuan_park.html')
+
+@app.route('/washingtonstatepark')
+def washingtonstatepark():
+	return render_template('Washington_State_park.html')
+
+@app.route('/washington')
+def washington():
+	return render_template('Washington.html')
+
+@app.route('/wildscenicriver')
+def wildscenicriver():
+	return render_template('Wild_Scenic_River.html')
+
+@app.route('/wyoming')
+def wyoming():
+	return render_template('Wyoming.html')
+
+@app.route('/yellowstonepark')
+def yellowstonepark():
+	return render_template('Yellowstone_park.html')
+
+
 if __name__ == '__main__':
 	app.run('107.170.39.210','80') # Run application
